4ea4fcdd1678d68263016db56fcec92946fb4001
Yasir Gaji <gajiyasir@gmail.com>
2023-03-15 13:02:37 +0100

    re-rendering the image

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
               maxWidth="100px"
               display="inline-block"
               borderRadius="full"
-              src={useColorModeValue("/images/me.jpg", "/images/me.jpg")}
+              src="/images/me.jpg"
               alt="Yasir Gaji"
             />
             </Center>
